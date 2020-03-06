46306f3d30ba8db607ee1873c92707a1fdd29405
BlackTim8212 <60044972+BlackTim8212@users.noreply.github.com>
2020-03-05 18:08:58 -0800

    SwitchToCurveDrive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,8 +10,13 @@ import edu.wpi.first.wpilibj.TimedRobot;
 //import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 //import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.XboxController;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX; 
-import com.ctre.phoenix.motorcontrol.ControlMode; 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import frc.robot.Test;
 
@@ -19,10 +24,11 @@ public class Robot extends TimedRobot {
 
   public static Test test = new Test(); 
 
-  private final VictorSPX mRightMaster = new VictorSPX(1);
-  private final VictorSPX mRightSlave = new VictorSPX(2);
-  private final VictorSPX mLeftMaster = new VictorSPX(3);
-  private final VictorSPX mLeftSlave = new VictorSPX(4);
+  private final WPI_VictorSPX mRightMaster = new WPI_VictorSPX(1);
+  private final WPI_VictorSPX mRightSlave = new WPI_VictorSPX(2);
+  private final WPI_VictorSPX mLeftMaster = new WPI_VictorSPX(3);
+  private final WPI_VictorSPX mLeftSlave = new WPI_VictorSPX(4);
+  private DifferentialDrive m_drive = new DifferentialDrive(mLeftMaster, mRightMaster);
 
  private final Joystick m_stick = new Joystick(0);
  
@@ -31,6 +37,18 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() { 
     CameraServer.getInstance().startAutomaticCapture();
+    mRightSlave.follow(mRightMaster);
+    mLeftSlave.follow(mLeftMaster);
+
+    mLeftMaster.setInverted(false);
+    mLeftSlave.setInverted(InvertType.FollowMaster);
+    mLeftMaster.setNeutralMode(NeutralMode.Brake);
+
+
+    mRightMaster.setInverted(false);
+    mRightMaster.setInverted(InvertType.FollowMaster);
+    mRightMaster.setNeutralMode(NeutralMode.Brake);
+
   }
   @Override
   public void robotPeriodic() {
@@ -49,13 +67,16 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     // control of robot, output=input^3
+    /*
     if(Math.abs(m_stick.getX()) >= 0.1 || Math.abs(m_stick.getY()) >= 0.1) {
+      
       rightMotors(Math.pow(m_stick.getX() - m_stick.getY(), 3));
       leftMotors(Math.pow(m_stick.getX() + m_stick.getY(), 3));
-
-     
+      */
+      m_drive.curvatureDrive(m_stick.getRawAxis(1), m_stick.getRawAxis(4)*.6, m_stick.getRawButton(6));
+      
     }
-  }
+  
 
   @Override
   public void testPeriodic() {
